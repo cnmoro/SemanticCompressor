78a2562fc03bdb29b94354e35cea262b2477f32c
Carlo Moro <carlo.moro@wises.com.br>
2025-04-15 13:24:16 -0300

    bump 2.2; add punk_tab nltk

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='semantic_compressor',
-    version='2.0',
+    version='2.2',
     author='Carlo Moro',
     author_email='cnmoro@gmail.com',
     description="Semantic text compression",
